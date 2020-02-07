57e3ecaa51217461fe3eac68fabe727d6eb72d6a
lucas.amorim <lucas.amorim@nubank.com.br>
2020-02-07 13:22:27 -0300

    Refactoring for hot_reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: ListaTransferencias(),
-          appBar: AppBar(
-            title: Text('Transferências'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-          ),
-        ),
-      ),
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: ListaTransferencias()
     );
-
-class Transferencia {
-  final double valor;
-  final String conta;
-
-  Transferencia(this.valor, this.conta);
+  }
 }
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(200.0, '0001')),
-        ItemTransferencia(Transferencia(100.0, '0001')),
-        ItemTransferencia(Transferencia(50.25, '0001'))
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(200.0, '0001')),
+          ItemTransferencia(Transferencia(100.0, '0001')),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
   final Transferencia _transferencia;
-
   ItemTransferencia(this._transferencia);
 
   @override
@@ -51,4 +47,9 @@ class ItemTransferencia extends StatelessWidget {
   }
 }
 
+class Transferencia {
+  final double valor;
+  final String conta;
 
+  Transferencia(this.valor, this.conta);
+}
