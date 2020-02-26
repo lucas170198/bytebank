d5e7e14f3738de4d8e62deee2e32881ff081cb01
lucas170198 <lucassilvaamorim1998@gmail.com>
2020-02-26 20:31:11 -0300

    WIP dashboard

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+const String _appBarText = 'Dashboard';
+
+const String _logoBytebank = 'images/bytebank_logo.png';
+
+const double _paddingSize = 8.00;
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_appBarText),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(_paddingSize),
+            child: Image.asset(_logoBytebank),
+          ),
+          DashboardBotoes(),
+        ],
+      ),
+    );
+  }
+}
+
+class DashboardBotoes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        FeatureBotao('Contatos', Icons.people),
+        FeatureBotao('Transferêcias', Icons.attach_money)
+      ],
+    );
+  }
+}
+
+class FeatureBotao extends StatelessWidget {
+  final String _text;
+  final IconData _icon;
+
+  const FeatureBotao(this._text, this._icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(_paddingSize),
+      child: Container(
+        padding: EdgeInsets.all(_paddingSize),
+        height: 100,
+        width: 150,
+        color: Theme.of(context).primaryColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Icon(
+              _icon,
+              color: Theme.of(context).iconTheme.color,
+              size: 24.00,
+            ),
+            Text(
+              _text,
+              style: TextStyle(color: Colors.white, fontSize: 16.00),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
