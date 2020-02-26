191899cd14b91d79603aedda0a23881229253dbd
lucas170198 <lucassilvaamorim1998@gmail.com>
2020-02-26 20:28:24 -0300

    Refactoring dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bytebank/screens/transferencias/lista.dart';
+import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -8,27 +8,11 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              child: Column(
-                children: <Widget>[
-                  Icon(Icons.people),
-                  Text('Contatos'),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: Dashboard(),
       theme: ThemeData(
         primaryColor: Colors.green[900],
         accentColor: Colors.blueAccent[700],
+        iconTheme: IconThemeData(color: Colors.white),
         buttonTheme: ButtonThemeData(
             buttonColor: Colors.blueAccent[700],
             textTheme: ButtonTextTheme.primary),
